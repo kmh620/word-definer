19effe879c382c1ebfa2ee34d2970576bda9f02a
katrina hockman <khockman90@hotmail.com>
2018-11-30 08:39:54 -0800

    Add initial files

---
+++ lib/word.rb
@@ -0,0 +1,25 @@
+class Word
+
+  attr_accessor(:word)
+
+  @@list = []
+
+  def initialize(word)
+    @word = word
+  end
+
+  def case_word
+    @@list.push(word.downcase.capitalize)
+  end
+
+
+
+
+
+
+
+
+
+
+
+end
