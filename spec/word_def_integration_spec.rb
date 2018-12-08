2baf15d24c0b54eb304e741c6fea41b364cc99be
Katrina Hockman <khockman90@hotmail.com>
2018-12-08 13:05:23 -0800

    Add routing fix, goes to word.id page, still no definition page shown

--- spec/word_def_integration_spec.rb
+++ spec/word_def_integration_spec.rb
@@ -1,15 +1,24 @@
-# require('word')
-# require('capybara/rspec')
-# require('./app')
-#
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
-#
-# describe('Adding a word', {:type => :feature}) do
-#   it('User adds word to dictionary') do
-#     visit('/')
-#     fill_in('input-word', :with => 'table')
-#     click_button('Add')
-#     expect(page).to have_content("Table")
-#   end
-# end
+require('word')
+require('capybara/rspec')
+require('./app')
+
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('Adding a word', {:type => :feature}) do
+  it('User adds word to dictionary') do
+    visit('/')
+    fill_in('input-word', :with => 'table')
+    click_button('Add')
+    expect(page).to have_content("Table")
+  end
+end
+
+describe('Adding a definition', {:type => :feature}) do
+  it('User adds definition(s) to word') do
+    visit('/')
+    fill_in('input-definition', :with => 'this is a definition')
+    click_button('Add Definition')
+    expect(page).to have_content("this is a definition")
+  end
+end
